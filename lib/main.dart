05b81aa2a9cb89361102f73ca9ceb8478ef7dd44
rmutr <sanya.sam@rmutr.ac.th>
2020-05-01 16:38:07 +0700

    UI update.

--- lib/main.dart
+++ lib/main.dart
@@ -3,109 +3,271 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: DigitalLock(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+//-----------------------------------------------------------------------------
+class DigitalLock extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _DigitalLockState createState() => _DigitalLockState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _DigitalLockState extends State<DigitalLock> {
+//-----------------------------------------------------------------------------
+  String tx_buff_str = "";
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+//-----------------------------------------------------------------------------
+  void keepinTxBuff(String pstr) {
+    if (tx_buff_str.length < 10) {
+      tx_buff_str += pstr;
+    }
+    print(tx_buff_str);
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+  void sendTxBuff(String pstr) {
+    print(pstr);
+  }
+
+  Widget dlBtn_0() {
+    return Container(
+      width: 80.0,
+      height: 50.0,
+      child: RaisedButton(
+          child: Text("0"),
+          onPressed: () {
+            this.keepinTxBuff("0");
+          }),
+    );
+  }
+
+  Widget dlBtn_1() {
+    return Container(
+      width: 80.0,
+      height: 50.0,
+      child: RaisedButton(
+          child: Text("1"),
+          onPressed: () {
+            this.keepinTxBuff("1");
+          }),
+    );
+  }
+
+  Widget dlBtn_2() {
+    return Container(
+      width: 80.0,
+      height: 50.0,
+      child: RaisedButton(
+          child: Text("2"),
+          onPressed: () {
+            this.keepinTxBuff("2");
+          }),
+    );
+  }
+
+  Widget dlBtn_3() {
+    return Container(
+      width: 80.0,
+      height: 50.0,
+      child: RaisedButton(
+          child: Text("3"),
+          onPressed: () {
+            this.keepinTxBuff("3");
+          }),
+    );
+  }
+
+  Widget dlBtn_4() {
+    return Container(
+      width: 80.0,
+      height: 50.0,
+      child: RaisedButton(
+          child: Text("4"),
+          onPressed: () {
+            this.keepinTxBuff("4");
+          }),
+    );
+  }
+
+  Widget dlBtn_5() {
+    return Container(
+      width: 80.0,
+      height: 50.0,
+      child: RaisedButton(
+          child: Text("5"),
+          onPressed: () {
+            this.keepinTxBuff("5");
+          }),
+    );
+  }
+
+  Widget dlBtn_6() {
+    return Container(
+      width: 80.0,
+      height: 50.0,
+      child: RaisedButton(
+          child: Text("6"),
+          onPressed: () {
+            this.keepinTxBuff("6");
+          }),
+    );
+  }
+
+  Widget dlBtn_7() {
+    return Container(
+      width: 80.0,
+      height: 50.0,
+      child: RaisedButton(
+          child: Text("7"),
+          onPressed: () {
+            this.keepinTxBuff("7");
+          }),
+    );
+  }
+
+  Widget dlBtn_8() {
+    return Container(
+      width: 80.0,
+      height: 50.0,
+      child: RaisedButton(
+          child: Text("8"),
+          onPressed: () {
+            this.keepinTxBuff("8");
+          }),
+    );
+  }
+
+  Widget dlBtn_9() {
+    return Container(
+      width: 80.0,
+      height: 50.0,
+      child: RaisedButton(
+          child: Text("9"),
+          onPressed: () {
+            this.keepinTxBuff("9");
+          }),
+    );
+  }
+
+  Widget dlBtnCancel() {
+    return Container(
+      width: 80.0,
+      height: 50.0,
+      child: RaisedButton(
+          child: Text("Cancel"),
+          onPressed: () {
+            tx_buff_str = "";
+          }),
+    );
+  }
+
+  Widget dlBtnOk() {
+    return Container(
+      width: 80.0,
+      height: 50.0,
+      child: RaisedButton(
+          child: Text("Ok"),
+          onPressed: () {
+            this.sendTxBuff("C2-" + tx_buff_str);
+          }),
+    );
+  }
+
+  Widget dlBtnStop() {
+    return Container(
+      width: 100.0,
+      height: 50.0,
+      child: RaisedButton(
+          child: Text("Stop"),
+          onPressed: () {
+            this.sendTxBuff("C0-" + tx_buff_str);
+          }),
+    );
+  }
+
+  Widget dlBtnStart() {
+    return Container(
+      width: 100.0,
+      height: 50.0,
+      child: RaisedButton(child: Text("Start"), onPressed: () {
+            this.sendTxBuff("C1-" + tx_buff_str);
+
+      }),
+    );
+  }
+
+  Widget dlBtnChangePincode() {
+    return Container(
+      width: 100.0,
+      height: 50.0,
+      child: RaisedButton(child: Text("Change Pincode"), onPressed: () {}),
+    );
+  }
+
+  Widget dlBtnAlarm() {
+    return Container(
+      width: 100.0,
+      height: 50.0,
+      child: RaisedButton(child: Text("Alarm"), onPressed: () {}),
+    );
+  }
+
+  Widget dlKeyPad() {
+    return Column(
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            dlBtn_1(),
+            dlBtn_2(),
+            dlBtn_3(),
+            dlBtnStop(),
           ],
         ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            dlBtn_4(),
+            dlBtn_5(),
+            dlBtn_6(),
+            dlBtnStart(),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            dlBtn_7(),
+            dlBtn_8(),
+            dlBtn_9(),
+            dlBtnChangePincode(),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            dlBtnCancel(),
+            dlBtn_0(),
+            dlBtnOk(),
+            dlBtnAlarm(),
+          ],
+        ),
+      ],
+    );
+  }
+
+//-----------------------------------------------------------------------------
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("RmutR Digital Lock V1"),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: dlKeyPad(),
     );
   }
 }
+
