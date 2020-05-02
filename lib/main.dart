3118d413d5bc243d733640c714d05da3bc6cab64
rmutr <sanya.sam@rmutr.ac.th>
2020-05-02 17:03:24 +0700

    Add bluetooth communitation.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+import 'dart:convert' show utf8;
+
 import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
 
 void main() => runApp(MyApp());
 
@@ -22,15 +26,121 @@ class _DigitalLockState extends State<DigitalLock> {
   String tx_buff_str = "";
 
 //-----------------------------------------------------------------------------
+  final String SERVICE_UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
+  final String CHARACTERISTIC_UUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
+  final String TARGET_DEVICE_NAME = "Digital Lock V1";
+
+  FlutterBlue flutterBlue = FlutterBlue.instance;
+  StreamSubscription<ScanResult> scanSubScription;
+
+  BluetoothDevice targetDevice;
+  BluetoothCharacteristic targetCharacteristic;
+
+  String connectionText = "";
+
+  @override
+  void initState() {
+    super.initState();
+    startScan();
+  }
+
+  startScan() {
+    setState(() {
+      connectionText = "01 Start Scanning";
+      print(connectionText);
+    });
+
+    scanSubScription = flutterBlue.scan().listen((scanResult) {
+      if (scanResult.device.name == TARGET_DEVICE_NAME) {
+        print('02 DEVICE found');
+        stopScan();
+        setState(() {
+          connectionText = "03 Found Target Device";
+          print(connectionText);
+        });
+
+        targetDevice = scanResult.device;
+        connectToDevice();
+      }
+    }, onDone: () => stopScan());
+  }
+
+  stopScan() {
+    scanSubScription?.cancel();
+    scanSubScription = null;
+  }
+
+  connectToDevice() async {
+    if (targetDevice == null) return;
+
+    setState(() {
+      connectionText = "04 Device Connecting";
+      print(connectionText);
+    });
+
+    await targetDevice.connect();
+    print('05 DEVICE CONNECTED');
+    setState(() {
+      connectionText = "06 Device Connected";
+      print(connectionText);
+    });
+
+    discoverServices();
+  }
+
+  disconnectFromDevice() {
+    if (targetDevice == null) return;
+
+    targetDevice.disconnect();
+
+    setState(() {
+      connectionText = "07 Device Disconnected";
+      print(connectionText);
+    });
+  }
+
+  discoverServices() async {
+    if (targetDevice == null) return;
+
+    print("08");
+    List<BluetoothService> services = await targetDevice.discoverServices();
+    services.forEach((service) {
+      print("09 " + SERVICE_UUID + " == " + service.uuid.toString());
+      // do something with service
+      if (service.uuid.toString() == SERVICE_UUID) {
+        print("10");
+        service.characteristics.forEach((characteristic) {
+          if (characteristic.uuid.toString() == CHARACTERISTIC_UUID) {
+            print("11");
+            targetCharacteristic = characteristic;
+            writeData("Hi there, ESP32!!");
+            setState(() {
+              connectionText = "12 All Ready with ${targetDevice.name}";
+              print(connectionText);
+            });
+          }
+        });
+      }
+    });
+  }
+
+  writeData(String data) {
+    if (targetCharacteristic == null) return;
+
+    List<int> bytes = utf8.encode(data);
+    targetCharacteristic.write(bytes);
+  }
+
+//-----------------------------------------------------------------------------
   void keepinTxBuff(String pstr) {
     if (tx_buff_str.length < 10) {
       tx_buff_str += pstr;
     }
-    print(tx_buff_str);
   }
 
   void sendTxBuff(String pstr) {
     print(pstr);
+    writeData(pstr);
   }
 
   Widget dlBtn_0() {
@@ -193,10 +303,11 @@ class _DigitalLockState extends State<DigitalLock> {
     return Container(
       width: 100.0,
       height: 50.0,
-      child: RaisedButton(child: Text("Start"), onPressed: () {
+      child: RaisedButton(
+          child: Text("Start"),
+          onPressed: () {
             this.sendTxBuff("C1-" + tx_buff_str);
-
-      }),
+          }),
     );
   }
 
@@ -270,4 +381,3 @@ class _DigitalLockState extends State<DigitalLock> {
     );
   }
 }
-
